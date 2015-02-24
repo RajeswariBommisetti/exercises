0bbadd0a3684187d8ccca492adad48eedeb3d205
rajeswaribommisetti <rajeshwari.iiit@gmail.com>
2015-02-24 12:03:08 +0000

    exercise

---
+++ sample_app/test/test_helper.rb
@@ -0,0 +1,13 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+  # Add more helper methods to be used by all tests here...
+end
